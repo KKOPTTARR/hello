8b7a5852594e849069293cd0f66c8584b9fd14e1
KKOPTTARR <2540121298@qq.com>
2020-03-10 10:34:46 +0800

    敲代码.c

--- 敲代码/敲代码/敲代码.c
+++ 敲代码/敲代码/敲代码.c
@@ -1,22 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	printf("你愿意敲代码吗？1表示愿意，0表示不愿意：");
+	printf("你愿意敲代码吗？0表示愿意，1表示不愿意:\n");
 	
 	int choice = 0;
-
-	scanf("%d", &choice);
-	//&叫做取地址符号
 	
-	if (choice == 1)
+	scanf("%d", &choice);//&叫做取地址符号
+
+	if (choice == 1)//==表示取相等
+		            //=表示赋值
 	{
 		printf("你会拿到好offer！\n");
 	}
 	else
 	{
-		printf("回家卖红薯吧！\n");
+		printf("回家卖红薯吧!\n");
 	}
 	return 0;
-
 }
