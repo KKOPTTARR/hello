22c7cab49ee4b49d54f197ee5c1923d9543a5b71
KKOPTTARR email=2540121298@qq.com <2540121298@qq.com>
2020-05-11 21:49:54 +0800

    bit9

---
+++ 结构体/结构体/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<Windows.h>
+
+
+//结构体
+
+//定义一个结构体类型
+typedef struct Stu
+{
+	char name[20];//名字 
+	int age;//年龄
+	char sex[5];//性别 
+	char id[20];//学号
+}
+Stu;//分号不能丢
+
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
